7d2a3659c105f90bf81b1a16bf1229ff1f154a61
God of war <9483474089@qq.com>
2020-09-21 14:59:44 +0800

    初步实现框架功能

--- framework.py
+++ framework.py
@@ -8,23 +8,60 @@
     4.web服务器结构组装成响应报文发动给浏览器
 
 内部构造：
-
-
-
 """
 import time
 
 
 # 获取首页数据
 def index():
-    pass
+    # 响应状态
+    status = "200 OK"
+    # 响应头
+    response_header = [("Server", "PWS2.0")]
+    # 处理后的数据
+    data = time.ctime()
+
+    return status, response_header, data
+
+
+# 获取login页面
+def login():
+    # 响应状态
+    status = "200 OK"
+    # 响应头
+    response_header = [("Server", "PWS2.0")]
+    # 处理后的数据
+    data = time.ctime()
+
+    return status, response_header, data
 
 
 # 没有找到动态资源
 def not_found():
-    pass
+    # 响应状态
+    status = "404 Not Found"
+    # 响应头
+    response_header = [("Server", "PWS2.0")]
+    # 处理后的数据
+    data = "not found"
+
+    return status, response_header, data
 
 
 # 处理动态资源请求
 def handle_request(env):
-    pass
+    # 获取动态资源请求路径
+    request_path = env["request_path"]
+    print("接收到的动态资源请求:", request_path)
+
+    if request_path == "/index.html":
+        # 获取首页数据
+        result = index()
+        return result
+    elif request_path == "/login.html":
+        result = login()
+        return result
+    else:
+        # 没有找到资源
+        result = not_found()
+        return result
