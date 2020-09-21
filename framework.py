1d2ca4e90283fe2a40a308da28259eeeccbaf112
God of war <9483474089@qq.com>
2020-09-21 16:01:49 +0800

    装饰器方法添加路由

--- framework.py
+++ framework.py
@@ -12,7 +12,29 @@
 import time
 
 
+# 定义路由列表
+route_list = []
+
+
+# 定义带参数的装饰器
+def route(path):
+    # 装饰器
+    def decorator(func):
+        # 当执行装饰器装饰指定函数的时候，把路径和函数添加到路由列表
+        route_list.append((path, func))
+
+        def inner():
+            # 执行指定函数
+            return func()
+
+        return inner
+
+    # 返回装饰器
+    return decorator
+
+
 # 获取login数据
+@route("/login.html")
 def login():
     # 响应状态
     status = "200 OK"
@@ -25,6 +47,7 @@ def login():
 
 
 # 获取首页数据
+@route("/index.html")
 def index():
     # 响应状态
     status = "200 OK"
@@ -43,23 +66,53 @@ def index():
     return status, response_header, result
 
 
+# # 获取个人中心数据
+# @route("/center.html")
+# def center():
+#     # 响应状态
+#     status = "200 OK";
+#     # 响应头
+#     response_header = [("Server", "PWS2.0")]
+#
+#     # 打开模板文件，读取数据
+#     with open("template/center.html", "r") as file:
+#         file_data = file.read()
+#
+#     # 处理后的数据, 从数据库查询
+#     data = time.ctime()
+#     # 替换模板文件中的模板遍历
+#     result = file_data.replace("{%content%}", data)
+#
+#     return status, response_header, result
+
+
 # 没有找到动态资源
 def not_found():
     # 响应状态
     status = "404 Not Found"
     # 响应头
-    response_header = [("Server", "PWS2.0")]
-    # 处理后的数据
-    data = "not found"
+    response_header = [("Server", "PWS1.0")]
 
-    return status, response_header, data
+    # 1. 打开模板文件，读取数据
+    with open("static/error.html", "r") as file:
+        file_data = file.read()
+
+    return status, response_header, file_data
+    # # 响应状态
+    # status = "404 Not Found"
+    # # 响应头
+    # response_header = [("Server", "PWS2.0")]
+    # # 处理后的数据
+    # data = "not found"
+    #
+    # return status, response_header, data
 
 
-# 定义路由列表
-route_list = [
-    ("/index.html", index),
-    ("/login.html", login)
-]
+# # 定义路由列表
+# route_list = [
+#     ("/index.html", index),
+#     ("/login.html", login)
+# ]
 
 
 # 处理动态资源请求
@@ -72,6 +125,10 @@ def handle_request(env):
     for path, func in route_list:
         if request_path == path:
             result = func()
+            return result
+        else:
+            # 没有动态资源
+            result = not_found()
             return result
 
     # if request_path == "/index.html":
