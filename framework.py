bb78d0c259c1653c44422411f0d07da438d2efd0
God of war <9483474089@qq.com>
2020-09-28 13:03:27 +0800

    Revert "我的网站V0"
    This reverts commit c86b27ab

--- framework.py
+++ framework.py
@@ -11,7 +11,6 @@
 """
 import time
 import pymysql
-import json
 
 
 # 定义路由列表
@@ -63,7 +62,7 @@ def index():
 
     # 处理后的数据, 从数据库查询
     conn = pymysql.connect(
-        host="10.170.34.56",
+        host="10.170.15.41",
         port=3306,
         user="root",
         password="287714",
@@ -99,84 +98,24 @@ def index():
     return status, response_header, result
 
 
-# 获取个人中心数据
-@route("/center.html")
-def center():
-    # 响应状态
-    status = "200 OK"
-    # 响应头
-    response_header = [("Server", "PWS2.0")]
-    # 打开模板，读文件数据
-    with open("template/center.html", "r", encoding='UTF-8') as file:
-        file_data = file.read()
-    # 替换模板文件
-    result = file_data.replace("{%content%}", "")
-
-    return status, response_header, result
-
-
-# 获取个人中心数据
-@route("/center_data.html")
-def center_data():
-    # 响应状态
-    status = "200 OK";
-    # 响应头
-    response_header = [("Server", "PWS2.0"), ("Content-Type", "text/html;charset=utf-8")]
-
-    conn = pymysql.connect(
-        host="10.170.34.56",
-        port=3306,
-        user="root",
-        password="287714",
-        database="stock_db",
-        charset="utf8"
-    )
-
-    # 获取游标
-    cursor = conn.cursor()
-    # 查询sql语句
-    sql = '''select i.code, i.short, i.chg, 
-                 i.turnover, i.price, i.highs, f.note_info 
-                 from info as i inner join focus as f on i.id = f.info_id;'''
-    # 执行sql语句
-    cursor.execute(sql)
-    # 获取结果集
-    result = cursor.fetchall()
-    # 关闭游标
-    cursor.close()
-    # 关闭数据库连接
-    conn.close()
-    # 个人中心数据列表
-    center_data_list = list()
-    # 遍历每一行数据转成字典
-    for row in result:
-        # 创建空的字典
-        center_dict = dict()
-        center_dict["code"] = row[0]
-        center_dict["short"] = row[1]
-        center_dict["chg"] = row[2]
-        center_dict["turnover"] = row[3]
-        center_dict["price"] = str(row[4])
-        center_dict["highs"] = str(row[5])
-        center_dict["note_info"] = row[6]
-        # 添加每个字典信息
-        center_data_list.append(center_dict)
-
-    # 把列表转为json格式字符串，并在控制台显示
-    json_str = json.dumps(center_data_list, ensure_ascii=False)
-    print(json_str)
-    return status, response_header, json_str
-
-    # 打开模板文件，读取数据
-    # with open("template/center.html", "r") as file:
-    #     file_data = file.read()
-    #
-    # # 处理后的数据, 从数据库查询
-    # data = time.ctime()
-    # # 替换模板文件中的模板遍历
-    # result = file_data.replace("{%content%}", data)
-    #
-    # return status, response_header, result
+# # 获取个人中心数据
+# @route("/center.html")
+# def center():
+#     # 响应状态
+#     status = "200 OK";
+#     # 响应头
+#     response_header = [("Server", "PWS2.0")]
+#
+#     # 打开模板文件，读取数据
+#     with open("template/center.html", "r") as file:
+#         file_data = file.read()
+#
+#     # 处理后的数据, 从数据库查询
+#     data = time.ctime()
+#     # 替换模板文件中的模板遍历
+#     result = file_data.replace("{%content%}", data)
+#
+#     return status, response_header, result
 
 
 # 没有找到动态资源
